ccd0bf44b109e8fbea2487745d81633fb84d6e4a
rkcoden <khanra4@farmingdale.edu>
2023-02-23 23:55:15 +0000

    DONE

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -4,9 +4,15 @@ public class IsSorted {
      * For example, {1,2,3,4} is storted but {1,2,4,3} is unsorted
      *
      * @param arr an int array
-     * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
+     * @return true if arr is sorted (all the numbers are in order). false if arr is
+     *         not sorted.
      */
-    public boolean checkSort(int[] arr){
-        return false;
+    public boolean checkSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
